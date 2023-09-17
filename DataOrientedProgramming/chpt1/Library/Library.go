c9a10dc41150d4758f4d293018fddd8f396372ca
BertVerhees <bert.verhees@rosa.nl>
2023-09-17 15:14:28 +0200

    restructured

--- DataOrientedProgramming/chpt1/Library/Library.go
+++
@@ -1,8 +0,0 @@
-package library
-
-/*The central part of the system design.*/
-
-type Library struct {
-	Name    string
-	Address string
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# DataOrientedProgramming
-# DataOrientedProgramming
